b3e7ca04eee73ebc1872bc2f3f241198a3b457f5
arabnejad <hamid.arabnejad@gmail.com>
2020-10-07 16:45:28 +0100

    Add run script to execute all c,python, and java program

---
+++ run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+OUTPUT_DIR="output_files"
+INPUT_DIR="input_files"
+# run c program
+gcc cannonsim.cpp -o cannonsim -lm
+./cannonsim -i $INPUT_DIR -o $OUTPUT_DIR
+
+# run python program
+python cannonsim.py --input_dir $INPUT_DIR --output_dir $OUTPUT_DIR
+
+
+# run java program
+export CLASSPATH="java_libs/commons-cli-1.3.1.jar:."
+javac cannonsim.java
+java cannonsim --input_dir $INPUT_DIR --output_dir $OUTPUT_DIR
+
+
+cat $OUTPUT_DIR/py_output.txt
+echo ""
+cat $OUTPUT_DIR/c_output.txt
+echo ""
+cat $OUTPUT_DIR/java_output.txt
